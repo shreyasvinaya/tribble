c62931a839eaf1198124c1a3b9a0c7a76841e125
rachit <rachau76@gmail.com>
2021-05-16 21:32:49 +0530

    improved UI

--- lib/screens/checkout.dart
+++ lib/screens/checkout.dart
@@ -16,222 +16,235 @@ class Checkout extends StatelessWidget {
     else{
       cost = perhr*num*24;
     }
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            SizedBox(height: 7.0,),
-            Align(
-              alignment: Alignment.center,
-              child: Container(
-                height: 60.0,
-                width: MediaQuery.of(context).size.width-30.0,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20.0),
-                  color: Colors.amber,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.where_to_vote_sharp,
-                      size: 30.0,
-                      color: Colors.black,
-                    ),
-                    SizedBox(width: 8.0,),
-                    Text("BITS Goa",
-                    style: TextStyle(
-                      fontSize: 25.0,
-                      fontWeight: FontWeight.bold
-                    ),),
-                  ],
-                ),
-              ),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/checkout.jpg'),
+              fit: BoxFit.cover,
             ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
-              child: Row(
-                children: [
-                  Container(
-                    height: 60.0,
-                    width: MediaQuery.of(context).size.width/2-15,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20.0),
-                      color: Colors.tealAccent,
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.watch_later_outlined,
-                          size: 35.0,
-                        ),
-                        SizedBox(width: 5.0,),
-                        Text("${dataReceived["number"]} ${dataReceived["timeType"]}",
-                        style: TextStyle(
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                        ),
-                      ],
-                    ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SizedBox(height: 7.0,),
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  height: 60.0,
+                  width: MediaQuery.of(context).size.width-30.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.0),
+                    color: Color(0xff0A2A73).withOpacity(0.8),
                   ),
-                  SizedBox(width: 15.0,),
-                  Container(
-                    height: 60.0,
-                    width: MediaQuery.of(context).size.width/2-20,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20.0),
-                      color: Colors.teal,
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text("₹",
-                        style: TextStyle(
-                          fontSize: 30.0,
-                          fontWeight: FontWeight.w600,
-                        ),
-                        ),
-                        SizedBox(width: 5.0,),
-                        Text("$cost",
-                        style:TextStyle(
-                          fontSize: 23.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                        )
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Align(
-              alignment: Alignment.center,
-              child: Container(
-                height: 150.0,
-                width: MediaQuery.of(context).size.width-30,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20.0),
-                  color: Colors.lightBlue,
-                ),
-                child: Row(
-                  children: [
-                    Image(
-                      height: 130.0,
-                      width: 200.0,
-                      image: AssetImage('assets/${GlobalConfiguration().get("image")}'),
-                    ),
-                    SizedBox(width: 50.0,),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text("${GlobalConfiguration().get("type")}",
-                        style: TextStyle(
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
-                          letterSpacing: 2.0,
-                        ),
-                        ),
-                        SizedBox(height: 30.0,),
-                        Text("₹ ${GlobalConfiguration().get("price")}",
-                        style: TextStyle(
-                          fontSize: 22.0,
-                          fontWeight: FontWeight.w600
-                        ),
-                        )
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Align(
-              alignment: Alignment.center,
-              child: Container(
-                height: 150.0,
-                width: MediaQuery.of(context).size.width-30,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20.0),
-                  color: Colors.grey,
-                ),
-                child: Padding(
-                  padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
                   child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text("Top places to\nvisit in Goa",
+                      Icon(
+                        Icons.where_to_vote_sharp,
+                        size: 30.0,
+                        color: Colors.white,
+                      ),
+                      SizedBox(width: 8.0,),
+                      Text("BITS Goa",
                       style: TextStyle(
-                        fontSize: 22.0,
-                        fontWeight: FontWeight.w500,
+                        fontSize: 25.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+                child: Row(
+                  children: [
+                    Container(
+                      height: 60.0,
+                      width: MediaQuery.of(context).size.width/2-15,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20.0),
+                        color: Colors.tealAccent,
                       ),
-                      ),
-                      // SizedBox(width: 15.0,),
-                      Container(
-                        height: 130.0,
-                        width: 170.0,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20.0),
-                          image: DecorationImage(
-                            image: AssetImage('assets/map.jpg'),
-                            fit: BoxFit.cover,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.watch_later_outlined,
+                            size: 35.0,
                           ),
-                        ),
+                          SizedBox(width: 5.0,),
+                          Text("${dataReceived["number"]} ${dataReceived["timeType"]}",
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 15.0,),
+                    Container(
+                      height: 60.0,
+                      width: MediaQuery.of(context).size.width/2-20,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20.0),
+                        color: Colors.teal,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("₹",
+                          style: TextStyle(
+                            fontSize: 30.0,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          ),
+                          SizedBox(width: 5.0,),
+                          Text("$cost",
+                          style:TextStyle(
+                            fontSize: 23.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  height: 150.0,
+                  width: MediaQuery.of(context).size.width-30,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.0),
+                    color: Color(0xff082932).withOpacity(0.9),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Image(
+                        height: 130.0,
+                        width: 200.0,
+                        image: AssetImage('assets/${GlobalConfiguration().get("image")}'),
+                      ),
+                      // SizedBox(width: 40.0,),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("${GlobalConfiguration().get("type")}",
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 2.0,
+                            color: Colors.white,
+                          ),
+                          ),
+                          SizedBox(height: 25.0,),
+                          Text("₹ ${GlobalConfiguration().get("price")}",
+                          style: TextStyle(
+                            fontSize: 22.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white
+                          ),
+                          )
+                        ],
                       ),
                     ],
                   ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(15.0, 0.0, 15.0, 0.0),
-              child: Row(
-                children: [
-                  InkWell(
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    child: Container(
-                      height: 90.0,
-                      width: MediaQuery.of(context).size.width/2-115,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30.0),
-                        color: Colors.grey,
-                      ),
-                      child: Icon(
-                        Icons.arrow_back_rounded,
-                        size: 35.0,
-                        color: Colors.black,
-                      ),
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  height: 150.0,
+                  width: MediaQuery.of(context).size.width-30,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.0),
+                    color: Color(0xff0B1F42).withOpacity(0.7),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text("Top places to\nvisit in Goa",
+                        style: TextStyle(
+                          fontSize: 22.0,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                        ),
+                        ),
+                        // SizedBox(width: 15.0,),
+                        Container(
+                          height: 130.0,
+                          width: 170.0,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20.0),
+                            image: DecorationImage(
+                              image: AssetImage('assets/map.jpg'),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  SizedBox(width: 15.0,),
-                  Container(
-                    height: 90.0,
-                    width: MediaQuery.of(context).size.width/2+70,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(25.0),
-                      color: Colors.black,
-                    ),
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        "Rent now",
-                        style: TextStyle(
-                          fontSize: 23.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(15.0, 0.0, 15.0, 0.0),
+                child: Row(
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Container(
+                        height: 90.0,
+                        width: MediaQuery.of(context).size.width/2-115,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30.0),
+                          color: Colors.grey,
+                        ),
+                        child: Icon(
+                          Icons.arrow_back_rounded,
+                          size: 35.0,
+                          color: Colors.black,
                         ),
                       ),
                     ),
-                  ),
-                ],
+                    SizedBox(width: 15.0,),
+                    Container(
+                      height: 90.0,
+                      width: MediaQuery.of(context).size.width/2+70,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(25.0),
+                        color: Colors.black,
+                      ),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                          "Rent now",
+                          style: TextStyle(
+                            fontSize: 23.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            SizedBox(height: 7.0,)
-          ],
+              SizedBox(height: 7.0,)
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/hours.dart
+++ lib/screens/hours.dart
@@ -5,54 +5,64 @@ class Hours extends StatelessWidget {
   List <String> hours = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text("Time Duration",
-        style: TextStyle(
-          fontSize: 25.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("Time Duration",
+          style: TextStyle(
+            fontSize: 25.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+          ),
+          ),
         ),
-        ),
-      ),
-      body: ListView.builder(
-        itemCount: hours.length,
-        itemBuilder: (context, index){
-          return InkWell(
-            onTap: () {
-              Navigator.pop(context, {"number": hours[index]});
-            },
-            child: Card(
-              elevation: 10.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20.0),
-              ),
-              margin: EdgeInsets.fromLTRB(60.0, 8.0, 60.0, 8.0),
-              color: Colors.grey,
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.watch_later_outlined,
-                      size: 30.0,
-                    ),
-                    SizedBox(width: 7.0,),
-                    Text("${hours[index]} hours",
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.w600,
-                      letterSpacing: 1.0,
-                    ),
-                    ),
-                  ],
-                ),
-              ),
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/watch.jpg'),
+              fit: BoxFit.cover,
             ),
-          );
-        },
+          ),
+          child: ListView.builder(
+            itemCount: hours.length,
+            itemBuilder: (context, index){
+              return InkWell(
+                onTap: () {
+                  Navigator.pop(context, {"number": hours[index]});
+                },
+                child: Card(
+                  elevation: 10.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  margin: EdgeInsets.fromLTRB(70.0, 8.0, 70.0, 8.0),
+                  color: Colors.grey.withOpacity(0.8),
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.watch_later_outlined,
+                          size: 33.0,
+                        ),
+                        SizedBox(width: 7.0,),
+                        Text("${hours[index]} hours",
+                        style: TextStyle(
+                          fontSize: 23.0,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 1.0,
+                        ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
       ),
     );
   }
