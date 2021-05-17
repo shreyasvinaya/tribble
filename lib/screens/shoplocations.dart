82fe17b27d5222afcc8d4e110cfc0b6eb7aa9b1c
l3limp <kakarvardaan@gmail.com>
2021-05-17 18:58:14 +0530

    maps on car selector working, shop name on top required

--- lib/screens/shoplocations.dart
+++ lib/screens/shoplocations.dart
@@ -105,7 +105,7 @@ class _MyHomePageState extends State<MyHomePage> {
         },
         child: InkWell(
             onTap: () {
-              Navigator.pushNamed(context, '/carselect', arguments: pickupLocations[index].locationCoordinates);
+              Navigator.pushNamed(context, '/carselect', arguments: pickupLocations[index].locationCoordinates );
               },
             child: Stack(
                 children: [
