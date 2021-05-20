dfe826c723a9c4e94678a21239b9be5c2d61c855
rachit <rachau76@gmail.com>
2021-05-20 16:36:10 +0530

    fixed bracket issues

--- lib/screens/carselector.dart
+++ lib/screens/carselector.dart
@@ -44,7 +44,6 @@ class Carselector extends State<HomeScreen> {
   }
 
   @override
-
   List<Car> cars = [
     Car(type: "Standard", price: "200", image: "standard.png"),
     Car(type: "MiniVan", price: "300", image: "minivan.png"),
@@ -52,15 +51,19 @@ class Carselector extends State<HomeScreen> {
     Car(type: "Luxury", price: "700", image: "luxury.png"),
     Car(type: "Sports", price: "1000", image: "sports.png"),
   ];
+
   Widget build(BuildContext context) {
     final authBloc = Provider.of<AuthBloc>(context);
-    LatLng coordinates = ModalRoute.of(context).settings.arguments;
+    LatLng coordinates = ModalRoute
+        .of(context)
+        .settings
+        .arguments;
     Set<Marker> _createMarker() {
       return {
         Marker(
-            markerId: MarkerId("marker"),
-            position: LatLng(coordinates.latitude, coordinates.longitude),
-            infoWindow: InfoWindow(title: 'Your Pickup Location'),
+          markerId: MarkerId("marker"),
+          position: LatLng(coordinates.latitude, coordinates.longitude),
+          infoWindow: InfoWindow(title: 'Your Pickup Location'),
         ),
       };
     }
@@ -68,153 +71,149 @@ class Carselector extends State<HomeScreen> {
       backgroundColor: Colors.black,
       body: SafeArea(
         child: StreamBuilder<User>(
-            stream: authBloc.currentUser,
-            builder: (context, snapshot) {
-              if (!snapshot.hasData) return CircularProgressIndicator();
-              print(snapshot.data.photoURL);
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(20.0, 0.0, 0.0, 5.0),
-                    child: FlatButton.icon(onPressed: () {
-                      Navigator.pop(context);
-                    },
-                        minWidth: MediaQuery.of(context).size.width-30,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20.0)
-                        ),
-                        color: Colors.grey[800],
-                        icon: Icon(
-                          Icons.add_location_sharp,
-                          size: 27.0,
-                          color: Colors.orange,
-                        ),
-                        label: Column(
-                          children: [
-                            Text("${GlobalConfiguration().get("location")}",
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                                letterSpacing: 1.0,
-                                color: Colors.white70,
-                              ),
-                            ),
-                            Text("\tChange Location!",
-                              style: TextStyle(
-                                  color: Colors.white
-                              ),),
-                            SizedBox(height: 5.0,),
-                          ],
-                        )),
-                  ),
-                  Expanded(
-              child: Stack(
-                children: [
-                  GoogleMap(
-                    initialCameraPosition: CameraPosition(
-                      target: LatLng(coordinates.latitude, coordinates.longitude),
-                      zoom: 13,
-                      tilt: 30,
-                      bearing: 20,
-                    ),
-                    markers: _createMarker(),
-                  ),
+          stream: authBloc.currentUser,
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) return CircularProgressIndicator();
+            print(snapshot.data.photoURL);
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
                 Padding(
-                  padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  padding: EdgeInsets.fromLTRB(20.0, 0.0, 0.0, 5.0),
+                  child: FlatButton.icon(onPressed: () {
+                    Navigator.pop(context);
+                  },
+                      minWidth: MediaQuery
+                          .of(context)
+                          .size
+                          .width - 30,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0)
+                      ),
+                      color: Colors.grey[800],
+                      icon: Icon(
+                        Icons.add_location_sharp,
+                        size: 27.0,
+                        color: Colors.orange,
+                      ),
+                      label: Column(
                         children: [
-                          CircleAvatar(
-                            backgroundImage: NetworkImage(snapshot
+                          Text("${GlobalConfiguration().get("location")}",
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: 1.0,
+                              color: Colors.white70,
+                            ),
+                          ),
+                          Text("\tChange Location!",
+                            style: TextStyle(
+                                color: Colors.white
+                            ),),
+                          SizedBox(height: 5.0,),
+                        ],
+                      )),
+                ),
+                Expanded(
+                  child: Stack(
+                    children: [
+                      GoogleMap(
+                        initialCameraPosition: CameraPosition(
+                          target: LatLng(
+                              coordinates.latitude, coordinates.longitude),
+                          zoom: 13,
+                          tilt: 30,
+                          bearing: 20,
+                        ),
+                        markers: _createMarker(),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                CircleAvatar(
+                                  backgroundImage: NetworkImage(snapshot
                                       .data.photoURL
                                       .replaceFirst('s96', 's400')),
-                            radius: 25.0,
-                          ),
-                        ],
-                      ),
-                      Container(
-                        height: 250.0,
-                        child: ListView.builder(
-                            scrollDirection: Axis.horizontal,
-                            itemCount: cars.length,
-                            itemBuilder: (context,index){
-                              return InkWell(
-                                onTap: () {
-                                  Navigator.pushNamed(context, '/timeselect');
-                                  GlobalConfiguration().updateValue("type", cars[index].type);
-                                  GlobalConfiguration().updateValue("price", cars[index].price);
-                                  GlobalConfiguration().updateValue("image", cars[index].image);
-                                },
-                                child: Container(
-                                  margin: EdgeInsets.fromLTRB(0.0, 10.0, 17.0, 17.0),
-                                  width: 200,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(15.0),
-                                    color: Colors.blue[100],
-                                  ),
-                                  child: Stack(
-                                    alignment: Alignment.topCenter,
-                                    children: [
-                                      Positioned(
-                                        bottom: 10.0,
-                                        child: Container(
-                                          height: 100.0,
-                                          width: 150.0,
-                                          decoration: BoxDecoration(
-                                            borderRadius: BorderRadius.circular(20.0),
-                                            boxShadow: [
-                                              BoxShadow(
-                                                color: Colors.black12,
-                                                offset: Offset(7.0,7.0),
-                                              ),
-                                            ],
-                                            color: Colors.blue[300],
-                                          ),
-                                          child: Padding(
-                                            padding: EdgeInsets.fromLTRB(30.0, 20.0, 0.0, 0.0),
-                                            child: Column(
-                                              crossAxisAlignment: CrossAxisAlignment.start,
-                                              children: [
-                                                Text("${cars[index].type}",
-                                                  style: TextStyle(
-                                                    fontSize: 20.0,
-                                                    letterSpacing: 1.5,
-                                                    fontWeight: FontWeight.bold,
-                                                  ),
+                                  radius: 25.0,
+                                ),
+                              ],
+                            ),
+                            Container(
+                              height: 250.0,
+                              child: ListView.builder(
+                                  scrollDirection: Axis.horizontal,
+                                  itemCount: cars.length,
+                                  itemBuilder: (context, index) {
+                                    return InkWell(
+                                      onTap: () {
+                                        Navigator.pushNamed(
+                                            context, '/timeselect');
+                                        GlobalConfiguration().updateValue(
+                                            "type", cars[index].type);
+                                        GlobalConfiguration().updateValue(
+                                            "price", cars[index].price);
+                                        GlobalConfiguration().updateValue(
+                                            "image", cars[index].image);
+                                      },
+                                      child: Container(
+                                        margin: EdgeInsets.fromLTRB(
+                                            0.0, 10.0, 17.0, 17.0),
+                                        width: 200,
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(
+                                              15.0),
+                                          color: Colors.blue[100],
+                                        ),
+                                        child: Stack(
+                                          alignment: Alignment.topCenter,
+                                          children: [
+                                            Positioned(
+                                              bottom: 10.0,
+                                              child: Container(
+                                                height: 100.0,
+                                                width: 150.0,
+                                                decoration: BoxDecoration(
+                                                  borderRadius: BorderRadius
+                                                      .circular(20.0),
+                                                  boxShadow: [
+                                                    BoxShadow(
+                                                      color: Colors.black12,
+                                                      offset: Offset(7.0, 7.0),
+                                                    ),
+                                                  ],
+                                                  color: Colors.blue[300],
                                                 ),
                                                 child: Padding(
                                                   padding: EdgeInsets.fromLTRB(
                                                       30.0, 20.0, 0.0, 0.0),
                                                   child: Column(
-                                                    crossAxisAlignment:
-                                                        CrossAxisAlignment
-                                                            .start,
+                                                    crossAxisAlignment: CrossAxisAlignment
+                                                        .start,
                                                     children: [
-                                                      Text(
-                                                        "${cars[index].type}",
+                                                      Text("${cars[index]
+                                                          .type}",
                                                         style: TextStyle(
                                                           fontSize: 20.0,
                                                           letterSpacing: 1.5,
-                                                          fontWeight:
-                                                              FontWeight.bold,
+                                                          fontWeight: FontWeight
+                                                              .bold,
                                                         ),
                                                       ),
-                                                      SizedBox(
-                                                        height: 15.0,
-                                                      ),
-                                                      Text(
-                                                        "₹${cars[index].price}/hr",
+                                                      SizedBox(height: 15.0,),
+                                                      Text("₹${cars[index]
+                                                          .price}/hr",
                                                         style: TextStyle(
                                                             fontSize: 22.0,
                                                             letterSpacing: 1.0,
-                                                            fontWeight:
-                                                                FontWeight.w600,
-                                                            color:
-                                                                Colors.white),
+                                                            fontWeight: FontWeight
+                                                                .w600,
+                                                            color: Colors.white
+                                                        ),
                                                       ),
                                                     ],
                                                   ),
@@ -225,26 +224,27 @@ class Carselector extends State<HomeScreen> {
                                               height: 110.0,
                                               width: 200.0,
                                               image: AssetImage(
-                                                  'assets/${cars[index].image}'),
+                                                  'assets/${cars[index]
+                                                      .image}'),
                                             ),
                                           ],
                                         ),
                                       ),
                                     );
-                                  }),
+                                  }
+                              ),
                             ),
                           ],
                         ),
                       ),
-                    ),
+                    ],
                   ),
-                ],
-              );
-            }),
                 ),
-                ],
-              ),
-            ),
+              ],
+            );
+          },
         ),
+      ),
+    );
   }
 }
