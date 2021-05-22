989ec18ef26cb65c95e9a9976e1251ff1c7ed5e7
l3limp <kakarvardaan@gmail.com>
2021-05-22 13:13:39 +0530

    ignore

--- lib/screens/confirmation.dart
+++ lib/screens/confirmation.dart
@@ -7,13 +7,14 @@ class Confirm extends StatefulWidget {
 
 class _ConfirmState extends State<Confirm> {
   Map dataReceived = {};
+
   @override
-  Widget build(BuildContext context) {
-    dataReceived = ModalRoute.of(context).settings.arguments;
-    return Scaffold(
-      body: Center(
-        child: Text("Your order has been confirmed\nCar Type: ${dataReceived["carType"]}\nPrice: ${dataReceived["price"]}\nPickUp Location: ${dataReceived["pickup"]}\nTime Duration: ${dataReceived["timeDuration"]}\nDate of Booking: ${dataReceived["date"]}"),
-      ),
-    );
-  }
+Widget build(BuildContext context) {
+  dataReceived = ModalRoute.of(context).settings.arguments;
+  return Scaffold(
+    body: Center(
+      child: Text("Your order has been confirmed\nCar Type: ${dataReceived["carType"]}\nPrice: ${dataReceived["price"]}\nPickUp Location: ${dataReceived["pickup"]}\nTime Duration: ${dataReceived["timeDuration"]}\nDate of Booking: ${dataReceived["date"]}"),
+    ),
+  );
+}
 }
