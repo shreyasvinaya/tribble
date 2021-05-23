87272beff62f2815306435e9b93e8ac70fbf2bc7
rachit <rachau76@gmail.com>
2021-05-23 18:38:40 +0530

    i forgot what i changed but yeah here we go

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,6 @@ import 'package:tribble/screens/shoplocations.dart';
 import 'package:tribble/screens/carselector.dart';
 import 'package:tribble/screens/checkout.dart';
 import 'package:tribble/screens/timeselector.dart';
-import 'package:tribble/screens/timetype.dart';
-import 'package:tribble/screens/hours.dart';
-import 'package:tribble/screens/days.dart';
 import 'package:global_configuration/global_configuration.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:tribble/blocs/auth_bloc.dart';
@@ -31,14 +28,11 @@ class Material1App extends StatelessWidget {
         theme: ThemeData(),
         darkTheme: ThemeData.dark(),
         routes: {
-          "/": (context) => Timeselector(),
+          "/": (context) => LoginScreen(),
           "/pickup": (context) => MyHomePage(),
           "/carselect": (context) => HomeScreen(),
           "/checkout": (context) => Checkout(),
           "/timeselect": (context) => Timeselector(),
-          // "/timetype": (context) => Timetype(),
-          // "/hours": (context) => Hours(),
-          // "/days": (context) => Days(),
           "/goaDest": (context) => Places(),
           "/confirm": (context) => Confirm(),
         },
